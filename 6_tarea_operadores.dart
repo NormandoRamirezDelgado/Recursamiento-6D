c4de1830a76aedbb428493ca5b0ae6d31d1973e9
Normando Ramírez <nramirezd@cbtis236.edu.mx>
2026-06-08 09:28:48 -0600

    .

--- 6_tarea_operadores.dart
+++ 6_tarea_operadores.dart
@@ -1,14 +1,13 @@
-// Hacer un programa que imprima la tabla del 5 completa del 0 al 10,
-// haciendo la multiplicación, suma, resta y división.
-// Ejemplo:
-// numero = 0;
-// print(5 * numero);
-// print(5 + numero);
-// print(5 - numero);
-// print(5 / numero);
-// numero = numero + 1
-// print(5 * numero); ...
-
 void main(List<String> args) {
-  
+  // Hacer un programa que imprima la tabla del 5 completa del 0 al 10,
+  // haciendo la multiplicación, suma, resta y división.
+  // Ejemplo:
+  // numero = 0;
+  // print(5 * numero);
+  // print(5 + numero);
+  // print(5 - numero);
+  // print(5 / numero);
+  // numero = numero + 1
+  // print(5 * numero); ...
+
 }
